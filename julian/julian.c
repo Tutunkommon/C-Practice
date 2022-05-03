c0fd2a56fcf49e8a3c1baa58136c05de1c8a753d
Tutunkommon <joe.jansen@gmail.com>
2022-05-03 19:34:40 -0400

    move constants

--- julian/julian.c
+++ julian/julian.c
@@ -1,5 +1,9 @@
-    long main (int y = 2022, int m = 5, int d = 3)
-    {
+    long main (){
+        
+    int y = 2022;
+    int m = 5;
+    int d = 3;
+    
 	y+=8000;
 	if(m<3) { y--; m+=12; }
 	return (y*365) +(y/4) -(y/100) +(y/400) -1200820
