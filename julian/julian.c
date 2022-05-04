02488fcfffee9bfa7e45789dd7827cccf7a2b962
Tutunkommon <joe.jansen@gmail.com>
2022-05-03 23:52:39 -0400

    cleanup random spaces

--- julian/julian.c
+++ julian/julian.c
@@ -1,12 +1,12 @@
-    
+
     #include <stdio.h>
-    
+
     int main (){
-        
+
     int y = 2022;
     int m = 5;
     int d = 3;
-    
+
 	y+=8000;
 	if(m<3) { y--; m+=12; }
 	long julian = (y*365) +(y/4) -(y/100) +(y/400) -1200820
@@ -17,3 +17,4 @@
     printf("Today is %ld \n", julian);
     return 0;
     }
+bash: line 1: Julian: command not found
